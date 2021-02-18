fbbb19358677913443781ec25ef33d832c44c302
andreagiussani <41335635+andreagiussani@users.noreply.github.com>
2021-02-18 11:17:47 +0100

    :zap: update pandas setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     url="https://github.com/andreagiussani/Applied_Machine_Learning_with_Python",
     license="BSD",
     packages=find_packages(),
-    install_requires=['xgboost==0.82', 'pandas==0.23.1', 'scikit-learn==0.21.3',
+    install_requires=['xgboost==0.82', 'pandas==1.1.1', 'scikit-learn==0.21.3',
                       'seaborn==0.9.0', 'catboost==0.16.2', 'gensim==3.8.1',
                       'nltk==3.4.5', 'matplotlib==3.1.0', 'wget==3.2',
                       'imbalanced-learn==0.5.0', 'tensorflow==2.4.0',
