a3a3b525e4c88cc1249020010c58991520866870
andreagiussani <GitHub2019>
2023-03-13 21:23:50 +0100

    :zap: fix requirements and setup for 0.2.0 version

--- setup.py
+++ setup.py
@@ -2,21 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="egeaML",
-    version="0.1.0",
+    version="0.2.0",
     author="Andrea Giussani",
     author_email="andrea.giussani@unibocconi.it",
     description=("A python library used in support of the Book"
                  "'Applied Machine Learning with Python'"),
     url="https://github.com/andreagiussani/Applied_Machine_Learning_with_Python",
     license="BSD",
-    packages=["egeaML"],
+    packages=find_packages(),
     install_requires=[
-        'pandas==1.5.3', 'scikit-learn==1.2.2', 'xgboost==0.82',
+        'pandas==1.5.3', 'scikit-learn==1.2.2', 'xgboost==1.7.4',
         'shap==0.41.0', 'catboost==1.1.1',
         'gensim==3.8.1', 'nltk==3.4.5',
-        'matplotlib==3.1.0', 'seaborn==0.9.0', 'wget==3.2',
+        'matplotlib==3.7.1', 'seaborn==0.9.0', 'wget==3.2',
         'imbalanced-learn==0.5.0', 'tensorflow==2.11.0',
     ],
     include_package_data=True,
-    classifiers=("Programming Language :: Python :: 3"),
 )
