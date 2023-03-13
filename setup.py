b33aa6741f194a6e7386a24f9764e343967c5880
andreagiussani <GitHub2019>
2023-03-13 21:57:51 +0100

    :zap: update setup 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="egeaML",
-    version="0.2.0",
+    version="0.2.3",
     author="Andrea Giussani",
     author_email="andrea.giussani@unibocconi.it",
     description=("A python library used in support of the Book"
@@ -11,11 +11,12 @@ setup(
     license="BSD",
     packages=find_packages(),
     install_requires=[
-        'pandas==1.5.3', 'scikit-learn==1.2.2', 'xgboost==1.7.4',
+        'pandas==1.5.3', 'scikit-learn==1.2.2',
         'shap==0.41.0', 'catboost==1.1.1',
         'gensim==3.8.1', 'nltk==3.4.5',
         'matplotlib==3.7.1', 'seaborn==0.9.0', 'wget==3.2',
         'imbalanced-learn==0.5.0', 'tensorflow==2.11.0',
+        'xgboost==1.7.4',
     ],
     include_package_data=True,
 )
